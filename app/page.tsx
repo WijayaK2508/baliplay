b11e3388391addeacc6c8ffd4d2bbe5719abc199
WijayaK2508 <wijayak2508@gmail.com>
2026-01-01 18:22:36 +0700

    change heylink to t.ly

--- app/page.tsx
+++ app/page.tsx
@@ -105,7 +105,7 @@ export default function BaliPlayWebsite() {
               <Link href="#contact" className="text-gray-700 hover:text-orange-600 transition-colors">
                 Contact
               </Link>
-                <Link href="https://heylink.me/Baliplay++/" passHref>
+                <Link href="https://t.ly/Baliplay" passHref>
                 <Button className="bg-gradient-to-r from-orange-500 to-blue-500 hover:from-orange-600 hover:to-blue-600">
                   Book Now
                 </Button>
@@ -182,7 +182,7 @@ export default function BaliPlayWebsite() {
               </div>
 
               <div className="flex flex-col sm:flex-row gap-4">
-                <Link href="https://heylink.me/Baliplay++/" passHref>
+                <Link href="https://t.ly/Baliplay" passHref>
                   <Button
                     size="lg"
                     className="bg-gradient-to-r from-orange-500 to-blue-500 hover:from-orange-600 hover:to-blue-600 text-lg px-8 py-3"
@@ -282,7 +282,7 @@ export default function BaliPlayWebsite() {
                       </div>
                     ))}
                   </div>
-                    <Link href="https://heylink.me/Baliplay++/" passHref>
+                    <Link href="https://t.ly/Baliplay" passHref>
                     <Button className="w-full bg-gradient-to-r from-orange-500 to-blue-500 hover:from-orange-600 hover:to-blue-600">
                       Book This Tour
                     </Button>
